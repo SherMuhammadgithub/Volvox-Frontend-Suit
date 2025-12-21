eff2e27f02dc2b43ac29f09b14c17d214c5412a5
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2025-12-21 10:29:15 +0500

    fix: change window.open to window.location.href for URL navigation

--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -48,7 +48,7 @@ const openUrl = (key: string) => {
   url.searchParams.set("user_name", userName);
   url.searchParams.set("user_email", userEmail);
   url.searchParams.set("user_id", userId);
-  window.open(url.toString(), "_blank");
+  window.location.href = url.toString();
 };
 
 export default function DashboardNavbar() {
